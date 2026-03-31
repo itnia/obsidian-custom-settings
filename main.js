afcbdb98dd61d72cd5b8f3184ef0515c8b53d88e
Sasha <itnia.aurey@outlook.com>
2026-03-31 21:21:40 +0300

    Update

--- main.js
+++ main.js
@@ -1,25 +1,55 @@
 const { Plugin } = require("obsidian");
 
-module.exports = class MyPlugin extends Plugin {
-  async onload() {
-    console.log("Plugin loaded");
+module.exports = class CodeBlockObserverPlugin extends Plugin {
+    onload() {
+        // наблюдаем за всем workspace
+        const workspaceEl = this.app.workspace.containerEl;
 
-    // Добавляем кнопку в левое меню
-    this.addRibbonIcon("dice", "My plugin", () => {
-      new Notice("Hello from plugin!");
-    });
+        const observer = new MutationObserver((mutations) => {
+            mutations.forEach(mutation => {
+                mutation.addedNodes.forEach(node => {
+                    if (!(node instanceof HTMLElement)) return;
 
-    // Добавляем команду
-    this.addCommand({
-      id: "my-command",
-      name: "Say Hello",
-      callback: () => {
-        new Notice("Hello 👋");
-      }
-    });
-  }
+                    let created = false;
 
-  onunload() {
-    console.log("Plugin unloaded");
-  }
+                    // ищем код-блоки с нужными классами
+                    if (node.classList.contains("HyperMD-codeblock") || 
+                        node.classList.contains("HyperMD-codeblock-begin")) {
+                        this.onCodeBlockCreated(node);
+                        created = true;
+                    }
+
+                    if (created) return;
+
+                    // внутри добавленных элементов
+                    node.querySelectorAll?.(".HyperMD-codeblock, .HyperMD-codeblock-begin").forEach(el => {
+                        this.onCodeBlockCreated(el);
+                        created = true;
+                    });
+
+                    if (created) return;
+                });
+            });
+        });
+
+        observer.observe(workspaceEl, {
+            childList: true,
+            subtree: true
+        });
+
+        this.cmObserver = observer;
+    }
+
+    onunload() {
+        this.cmObserver?.disconnect();
+    }
+
+    onCodeBlockCreated(el) {
+        // момент создания кода в редакторе
+        el.classList.add("code-block-processed");
+        console.log("Code block created:", el);
+
+        // TODO:
+        //  - скрыть апостравы и вывести окошко выбора языка
+    }
 };
